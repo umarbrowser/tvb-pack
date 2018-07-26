954ece61e99dba4f0d3e411694d74c1f085df544
umar abdullahi <umarbrowser20@gmail.com>
2018-07-26 15:01:03 +0100

    Fix some issues and update bash script

--- docker/tvb-build/tvb-build.sh
+++ docker/tvb-build/tvb-build.sh
@@ -3,29 +3,13 @@
 # Clear the terminal
 clear
 
-# Install linux-headers and some build tools
-apt-get -y update
-apt-get -y install linux-headers-amd64 build-essential libc-dev gcc
-
-# Configure conda-forge as one of conda channels
-conda config --add channels conda-forge
-# Install all the third party tools needed for tvb packages
-conda install networkx numpy numba numexpr matplotlib \
-                  pytest scikit-learn scipy cython \
-		  BeautifulSoup4 cfflib cherrypy formencode genshi \
-                  h5py networkx nibabel Pillow psutil pytest simplejson \
-		  sqlalchemy sqlalchemy-migrate
-# Install the remaining 3rd party tools that does not exit on any conda channels
-# via python pypi	  
-pip install jaraco.functools allensdk
-
 # Move to /opt directory and git clone tvb-data
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-data.git
 # Install it because it does not require any unittest
 cd /opt/tvb-data && python setup.py install
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
-cd /opt && rm -r /tvb-data
+cd /opt && rm -rf /tvb-data
 
 # Move to /opt directory and git clone tvb-geodesic
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-geodesic.git
@@ -33,7 +17,7 @@ cd /opt && git clone https://github.com/the-virtual-brain/tvb-geodesic.git
 cd /opt/tvb-geodesic && python setup.py install
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
-cd /opt && rm -r /tvb-geodesic
+cd /opt && rm -rf /tvb-geodesic
 
 # Move to /opt directory and git clone tvb-library
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-library.git
@@ -44,7 +28,7 @@ pytest tvb/test
 python setup.py install
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
-cd /opt && rm -r /tvb-library
+cd /opt && rm -rf /tvb-library
 
 # Move to /opt directory and git clone tvb-framework
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-framework.git
@@ -55,4 +39,4 @@ pytest tvb/tests/framework/interfaces/web/controllers/help_controller_test
 python setup.py install
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
-cd /opt && rm -r /tvb-framework
+cd /opt && rm -rf /tvb-framework
