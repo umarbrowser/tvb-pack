122896057fa10a4e999612d872cf8f5543231b81
umar abdullahi <umarbrowser20@gmail.com>
2018-07-29 14:18:17 +0100

    update and improve code quality

--- docker/tvb-build/tvb-build.sh
+++ docker/tvb-build/tvb-build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-
 # Clear the terminal
 clear
+
+# Create a virtual env for our building development
+virtualenv tvb_distribution
+# Activate it
+source tvb_distribution/bin/activate && pip install --upgrade pip && pip install wheel
 
 # Move to /opt directory and git clone tvb-data
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-data.git
 # Install it because it does not require any unittest
-cd /opt/tvb-data && python setup.py install
+cd /opt/tvb-data
+# build the .whl
+python setup.py bdist_wheel
+pip install dist/tvb_data-1.5.6-py2-none-any.whl
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
 cd /opt && rm -rf /tvb-data
@@ -14,7 +21,10 @@ cd /opt && rm -rf /tvb-data
 # Move to /opt directory and git clone tvb-geodesic
 cd /opt && git clone https://github.com/the-virtual-brain/tvb-geodesic.git
 # Install it because it does not require any unittest
-cd /opt/tvb-geodesic && python setup.py install
+cd /opt/tvb-geodesic
+# build the .whl
+python setup.py bdist_wheel
+pip install dist/tvb_gdist-1.5.6-cp27-cp27mu-linux_x86_64.whl
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
 cd /opt && rm -rf /tvb-geodesic
@@ -24,8 +34,9 @@ cd /opt && git clone https://github.com/the-virtual-brain/tvb-library.git
 # run the pytest for the require unittest
 cd /opt/tvb-library
 pytest tvb/test
-# Install it
-python setup.py install
+# build the .whl
+python setup.py bdist_wheel
+pip install dist/tvb_library-1.5.6-py2-none-any.whl
 # Remove the clone directory after everything, Because if we leave it making the image
 # bigger in size.
 cd /opt && rm -rf /tvb-library
@@ -35,15 +46,9 @@ cd /opt && git clone https://github.com/the-virtual-brain/tvb-framework.git
 # run the pytest for the require unittest
 cd /opt/tvb-framework
 pytest tvb/tests/framework/interfaces/web/controllers/help_controller_test
-# Install it
-python setup.py install
-
-# Create a virtual env for our building development
-virtualenv tvb_distribution
-# Activate it
-source tvb_distribution/bin/activate && pip install --upgrade pip && pip install wheel
 # build the .whl
 python setup.py bdist_wheel
-# install it on our virtual env.
-pip install dist/tvb-framework.whl
-
+pip install dist/tvb_framework-1.5.6-py2-none-any.whl
+# Remove the clone directory after everything, Because if we leave it making the image
+# bigger in size.
+cd /opt && rm -rf /tvb-framework
