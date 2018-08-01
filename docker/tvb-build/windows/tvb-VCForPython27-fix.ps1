7c36d910c4658b11d12e693d12eb729f5e807dff
umar abdullahi <umarbrowser20@gmail.com>
2018-08-01 14:34:27 +0100

    Fix error: Microsoft Visual C++ 9.0 is require when python setup.py bdist_wheel,and added forloop when installing .whl

---
+++ docker/tvb-build/windows/tvb-VCForPython27-fix.ps1
@@ -0,0 +1,25 @@
+﻿# Download the Microsoft Visual C++ 9.0 which is required.from http://aka.ms/vcpython27.
+Invoke-WebRequest -Uri https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi -OutFile c:\windows\temp\VCForPython27.msi -UseBasicParsing
+# Get the current date and time for wrinting log fle
+# with is a good practice for installing .msi package.
+$DataStamp = get-date -Format yyyyMMddTHHmmss
+# our logfile.
+$logFile = '{0}-{1}.log' -f "c:\windows\temp\VCForPython27.msi",$DataStamp
+# The commands for installing .msi file on powershell.
+# msiexec Provides the means to install, modify, and perform operations 
+# on Windows Installer from the command line.
+# Some msiexec ArgumentList means
+# i - Status messages.
+# qn - No UI(User interface)
+# L- Writes logging information into a logfile at the specified existing path
+# v - Verbose output.
+$MSIArguments = @(
+    "/i"
+    ('"{0}"' -f "c:\windows\temp\VCForPython27.msi")
+    "/qn"
+    "/norestart"
+    "/L*v"
+    $logFile
+)
+# Install our downloaded VCForPython27.ms fix.
+Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
--- docker/tvb-build/windows/tvb-build.cmd
+++ docker/tvb-build/windows/tvb-build.cmd
@@ -5,6 +5,11 @@ cls
 rem Move to C:\ directory and create a opt directory
 cd C:\ & mkdir opt
 
+rem fixing error: Microsoft Visual C++ 9.0 
+rem is required. Get it from http://aka.ms/vcpython27
+rem with is raise when setup.py bdist_wheel for tvb-geodesic and others.
+powershell C:\TVB\tvb-VCForPython27-fix.ps1
+
 rem Create a virtual env for our building development
 virtualenv tvb_distribution
 rem Activate it
@@ -15,7 +20,7 @@ cd C:\opt & git clone https://github.com/the-virtual-brain/tvb-data.git
 rem Install it because it does not require any unittest
 cd \tvb-data
 rem build the .whl
-python setup.py bdist_wheel & pip install dist\*.whl
+python setup.py bdist_wheel & for %x in (dist\*.whl) do python -m pip install %x
 rem Remove the clone directory after everything, Because if we leave it making the image
 rem bigger in size.
 cd C:\opt & rmdir tvb-data
@@ -25,7 +30,7 @@ cd C:\opt & git clone https://github.com/the-virtual-brain/tvb-geodesic.git
 rem Install it because it does not require any unittest
 cd C:\opt\tvb-geodesic
 rem build the .whl
-python setup.py bdist_wheel & pip install dist\*.whl
+python setup.py bdist_wheel & for %x in (dist\*.whl) do python -m pip install %x
 rem Remove the clone directory after everything, Because if we leave it making the image
 rem bigger in size.
 cd C:\opt & rmdir tvb-geodesic
@@ -35,7 +40,7 @@ cd C:\opt & git clone https://github.com/the-virtual-brain/tvb-library.git
 rem run the pytest for the require unittest
 cd C:\opt\tvb-library\ & pytest tvb\test
 rem build the .whl
-python setup.py bdist_wheel & pip install dist\*.whl
+python setup.py bdist_wheel & for %x in (dist\*.whl) do python -m pip install %x
 rem Remove the clone directory after everything, Because if we leave it making the image
 rem bigger in size.
 cd C:\opt & rmdir tvb-library
@@ -45,7 +50,7 @@ cd C:\opt & git clone https://github.com/the-virtual-brain/tvb-framework.git
 rem run the pytest for the require unittest
 cd C:\opt\tvb-framework\ & pytest tvb\tests\ramework\interfaces\web\controllers\help_controller_test
 rem build the .whl
-python setup.py bdist_wheel & pip install dist\*.whl
+python setup.py bdist_wheel & for %x in (dist\*.whl) do python -m pip install %x
 rem Remove the clone directory after everything, Because if we leave it making the image
 rem bigger in size.
 cd C:\opt\ & rmdir tvb-framework
